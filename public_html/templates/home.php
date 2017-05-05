e78e7aad0db060cf72c53590e19ff7535cefb784
Shihlin Lu <shihlinlu@gmail.com>
2017-05-05 11:12:15 -0600

    added home template

---
+++ public_html/templates/home.php
@@ -0,0 +1,8 @@
+<section class="welcome">
+	<div class="container">
+		<div class="jumbotron text-center">
+			<h1><i class="fa fa-microchip"></i> Welcome to the Angular 2 Spinup!</h1>
+			<p class="lead orange">This is a skeletal Angular 2 Front End</p>
+		</div>
+	</div>
+</section>
