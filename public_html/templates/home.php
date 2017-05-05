807e0651160ac37e6c573e01d4cfb5f7436f6456
Shihlin Lu <shihlinlu@gmail.com>
2017-05-05 12:20:46 -0600

    updated home template

--- public_html/templates/home.php
+++ public_html/templates/home.php
@@ -1,7 +1,7 @@
 <section class="welcome">
 	<div class="container">
 		<div class="jumbotron text-center">
-			<h1><i class="fa fa-microchip"></i> Welcome to the Angular 2 Spinup!</h1>
+			<h1><i class="fa fa-microchip"></i> Welcome to the Angular 2 Spinup! Hello, user!</h1>
 			<p class="lead orange">This is a skeletal Angular 2 Front End</p>
 		</div>
 	</div>
